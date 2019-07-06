0f9f36d64f94b5030049db967dee172511b51d68
davidecolombo <2599183+davidecolombo@users.noreply.github.com>
2019-07-06 14:00:21 +0200

    Added JUnit5 and isIPv4Address tests, ignored Eclipse .settings

--- src/main/java/space/davidecolombo/noip/utils/IpUtils.java
+++ src/main/java/space/davidecolombo/noip/utils/IpUtils.java
@@ -2,7 +2,6 @@ package space.davidecolombo.noip.utils;
 
 import java.util.regex.Pattern;
 
-import lombok.NonNull;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -11,7 +10,7 @@ public class IpUtils {
 	private static final Pattern IPV4_PATTERN =
 			Pattern.compile("^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
 
-	public static boolean isIPv4Address(@NonNull final String ip) {
-		return IPV4_PATTERN.matcher(ip).matches();
+	public static boolean isIPv4Address(final String ip) {
+		return ip == null ? false : IPV4_PATTERN.matcher(ip).matches();
 	}
 }
